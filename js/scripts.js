eae60066976671b3575ccf23589f242b0a1217aa
RoiEX <RoiEXLab@users.noreply.github.com>
2017-01-21 00:19:14 +0100

    Moved js to site where it's used

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,1 @@
-function hideShowInstallationText(which) {
-  [].forEach.call(document.getElementsByClassName("installation-instructions"), function(current){
-    if(current !== which){
-      current.style.display="none";
-    }
-  });
-  if(which){
-    which.style.display="block";
-  }
-}
+//Nothing here
